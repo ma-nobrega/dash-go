c7ec254edc23458161df5578b328726ce13ed732
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-09-24 09:07:42 -0300

    Add formatting to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import {AppProps} from 'next/app';
-import {ChakraProvider} from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,65 @@
-import {Flex, Input, FormLabel, FormControl, Button, Stack} from '@chakra-ui/react'
+import {
+  Flex,
+  Input,
+  FormLabel,
+  FormControl,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
 
-export default function Home() {
+export default function Home(): React.ReactNode {
   return (
-    <Flex width="100vw" height="100vh" alignItems="center" justifyContent="center">
-      <Flex as="form" width= "100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
         <Stack spacing="4">
           <FormControl>
             <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input name="email" type="email" id="email" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" size="lg"_hover={{
-              bgColor:'gray.900'
-            }}/>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+            />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="email">Senha</FormLabel>
-            <Input name="password" type="password" id="password" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" size="lg" _hover={{
-              bgColor:'gray.900'
-            }}/>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+            />
           </FormControl>
         </Stack>
-        <Button mt="6" colorScheme="pink" type="submit" size="lg">Entrar</Button>
+        <Button mt="6" colorScheme="pink" type="submit" size="lg">
+          Entrar
+        </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
